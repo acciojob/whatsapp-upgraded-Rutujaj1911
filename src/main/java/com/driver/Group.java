1e283a0db99a5569f67e1046d31f57dc944545cd
Rutuja <rutujajadhav896@gmail.com>
2023-02-01 17:15:02 +0530

    1st

--- src/main/java/com/driver/Group.java
+++ src/main/java/com/driver/Group.java
@@ -1,7 +1,34 @@
 package com.driver;
 
-public class Group {
-    private String name;
-    private int numberOfParticipants;
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Group {
+
+
+    public String name;
+    public int numberOfParticipants;
+
+    public Group(){
+
+    }
+    public Group(String name,int numberOfParticipants) {
+        this.name = name;
+        this.numberOfParticipants=numberOfParticipants;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfParticipants() {
+        return numberOfParticipants;
+    }
+
+    public void setNumberOfParticipants(int numberOfParticipants) {
+        this.numberOfParticipants = numberOfParticipants;
+    }
 }
---
+++ src/main/java/com/driver/WhatsappService.java
@@ -0,0 +1,36 @@
+package com.driver;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WhatsappService {
+
+    WhatsappRepository whatsappRepository=new WhatsappRepository();
+
+    public  boolean isUser(String mobile) {
+        return whatsappRepository.isUser(mobile);
+    }
+
+    public String createUser(String name, String mobile) {
+        whatsappRepository.createUser(name, mobile);
+        return "SUCCESS";
+    }
+
+    public Group createGroup(List<User> users) {
+        return whatsappRepository.createGroup(users);
+    }
+
+    public int createMessage(String content) {
+        return whatsappRepository.createMessage(content);
+    }
+
+    public int sendMessage(Message message, User sender, Group group) throws Exception {
+        return whatsappRepository.sendMessage(message, sender, group);
+    }
+
+    public String changeAdmin(User approver, User user, Group group) throws Exception {
+        return whatsappRepository.changeAdmin(approver, user, group);
+    }
+}
